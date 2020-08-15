2b677384383fc48f81187885494c0c58f7658281
Pushkar Tiwari <pushkartiwari33@gmail.com>
2020-08-16 01:48:54 +0530

    1. Added code to test REST API methods.
    2. Added code to validate API which returns country and country code.

--- testAPI/test_API.py
+++ testAPI/test_API.py
@@ -1,11 +1,14 @@
 import requests
 import json
+# import pytest_html
+import logging
 
-
+BASE_URL = 'https://reqres.in'
+page1 = {'page': 1}
+page2 = {'page': 2}
 def test_api_get():
-    BASE_URL = 'https://reqres.in'
     param = {'page':2}
-    resp = requests.get(BASE_URL+"/api/users",params=param)
+    resp = requests.get(BASE_URL+"/api/users",params=page2)
     assert (resp.status_code == 200), "Status code is not 200. Rather found : " + str(resp.status_code)
     print(resp.status_code)
 
@@ -27,10 +30,6 @@ def test_api_get():
     for i in record:
         print(i['id'], i['first_name'])
 
-
-
-
-
     # print('DATA-------: ', resp.json()['data'])
     for record in resp.json()['data']:
         if record['id'] == 12:
@@ -40,30 +39,73 @@ def test_api_get():
                 "Data not matched! Expected : Holt, but found : " + str(record['last_name'])
 
 def test_api_post():
-    data = {'name': 'John',
+
+    postdata = {'name': 'John',
             'job': 'QA'}
-    resp = requests.post(url="https://reqres.in/api/users", data=data)
-    data = resp.json()
-    print('DATA---------',data)
+    resp = requests.post(url=BASE_URL+"/api/users", data=postdata)
+    new_data = resp.json()
+    print('DATA---------',json.dumps(new_data,indent=4))
     assert (resp.status_code == 201), "Status code is not 201. Rather found : " \
                                       + str(resp.status_code)
-    assert data['name'] == "John", "User created with wrong name. \
-            Expected : John, but found : " + str(data['name'])
-    assert data['job'] == "QA", "User created with wrong job. \
-            Expected : QA, but found : " + str(data['name'])
+    assert new_data['name'] == "John", "User created with wrong name. \
+            Expected : John, but found : " + str(new_data['name'])
+    assert new_data['job'] == "QA", "User created with wrong job. \
+            Expected : QA, but found : " + str(new_data['name'])
 
-    data = {'id': '1',
-            'first_name': 'Pushkar'}
-    resp = requests.post(url="https://reqres.in/api/users", data=data)
-    print(resp.json())
+    resp = requests.get(url=BASE_URL+"/api/users", params=page1)
+    print(json.dumps(resp.json(),indent=4))
+    print('-------------------------------------------------')
+    resp = requests.get(url=BASE_URL + "/api/users", params=page2)
+    print(json.dumps(resp.json(), indent=4))
+
+def test_api_Put():
+    updatedata = {
+            "id": 1,
+            "email": "pushkartiwari@gmail.com",
+            "first_name": "Pushkar",
+            "last_name": "Tiwari",
+        }
+
+    resp = requests.put(url=BASE_URL+"/api/users", data=updatedata)
+    print(json.dumps(resp.json(),indent=4))
     print('-----------------')
-    resp = requests.get(url="https://reqres.in/api/users")
+    resp = requests.get(url=BASE_URL+"/api/users", params=page1)
     data = resp.json()["data"]
-    for i in range(len(resp.json()["data"])):
-        print(data[i])
+    print(json.dumps(data,indent=4))
 
-    for record in resp.json()['data']:
-        if record['id'] == 1:
-            assert record['first_name'] == "Pushkar",\
-                "Data not matched! Expected : Pushkar, but found : " + str(record['first_name'])
+    for record in data:
+        print(record["first_name"])
+
+
+def test_countryAPI():
+    url = "https://covid-19-data.p.rapidapi.com/help/countries"
+
+    querystring = {"format": "json"}
+
+    headers = {
+        'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
+        'x-rapidapi-key': "95bf7eb63bmshd85da20246a7e32p1a16a8jsncabf7d1e8798"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    assert response.status_code == 200
+    Resp = response.json()
+    print(type(Resp))
+    print(json.dumps(response.json(),indent=4))
+
+    country = []
+    countryCode = []
+
+    for i in range(len(Resp)):
+        myd = dict(Resp[i])
+        # print(myd)
+        country.append(myd['name'])
+        countryCode.append(myd['alpha2code'])
+        assert myd['name'] == country[i]
+        assert myd['alpha2code'] == countryCode[i]
+        countryDict = {country[i] : countryCode[i]}
+        print(countryDict)
+
+
 
